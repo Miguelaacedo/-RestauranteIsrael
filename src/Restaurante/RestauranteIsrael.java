e3f2358b05dd42f05c6f67dcda3f02ee77e3f63b
MiguelAcedo <daw101.2023@gmail.com>
2025-02-17 19:28:47 +0100

    Codigo Comentado

--- src/Restaurante/RestauranteIsrael.java
+++ src/Restaurante/RestauranteIsrael.java
@@ -1,29 +1,64 @@
 package Restaurante;
-
+/**
+ * La clase RestauranteIsrael gestiona los comensales del restaurante Israel
+ * 
+ * @author Miguel Acedo
+ * @version 1.0
+ */
 
 	public class RestauranteIsrael {
+		/** Representa el número de patatas */
 		private int patatas;
+		/** Representa el número de calamares */
 		private int calamares;
 		private static RestauranteIsrael miRestaurante;
-
+		/**
+		 * Contructor de la clase RestauranteIsrael
+		 * 
+		 * @param a Indica el número de patatas que hay en el stock
+		 * @param b Indica el número de calamares que hay en el stock
+		 */
 		public RestauranteIsrael(int a, int b) {
 			this.setPatatas(a);
 			this.setCalamares(b);
 		}
+		/**
+		 * Método por el que se conoce el número máximo de comensales en función del
+		 * número de patatas en stock
+		 * 
+		 * @return Devuelve el número de comensales
+		 */
 
 		int cantP() {
 			int a = this.getPatatas() * 3;
 			return a;
 		}
 
+		/**
+		 * Método por el que se conoce el número máximo de comensales en función del
+		 * número de calamares en stock
+		 * 
+		 * @return Devuelve el número de comensales
+		 */
 		int cantC() {
 			int ch = this.getCalamares() * 6;
 			return ch;
 		}
+		/**
+		 * Método por el que se añade la cantidad de calamares recibidos en un pedido
+		 * 
+		 * @param c cantidad de calamares a añadir al stock
+		 */
 
 		public void addCalamares(int c) {
 			this.setCalamares(this.getCalamares() + c);
 		}
+		/**
+		 * Método por el que se añade la cantidad de patatas recibidos en un pedido
+		 * 
+		 * @param d cantidad de patatas a añadir al stock
+		 */
+		
 
 		public void addPatatas(int d) {
 			this.setPatatas(this.getPatatas() + d);
